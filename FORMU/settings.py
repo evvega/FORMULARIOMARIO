dc230a79153448466b1ed81b2fa2523824b93de8
evelin <evvega.18@hotmail.com>
2017-10-23 10:01:23 -0500

    Se añaden los modelos de empresa y de informacion adicional de la aplicacion Empresa

--- FORMU/settings.py
+++ FORMU/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aplicaciones.cliente'
+    'aplicaciones.cliente',
+    'aplicaciones.empresa',
 
 
 ]
--- aplicaciones/empresa/models.py
+++ aplicaciones/empresa/models.py
@@ -14,3 +14,18 @@ class ENTERPRISE(models.Model):
             ('CARTAGEA_DE_INDIAS', 'Cartagena de indias'))
     TYPE_CITY_COMPANY = models.TextField(max_length=1, choices=CITY)
     ADDRESS = models.CharField(max_length=80)
+
+    def __str__(self):
+        return '{}'.format(self.NAME)
+
+
+
+class ADDINFORMATION(models.Model):
+    client2 = models.ForeignKey(ENTERPRISE, null=True, blank=True)
+
+    WAY_TO_PAY =(('TCN','Tarjeta de Crédito Nacional'),('VS','Visa'),('MC','Mastercard'),('AE','American Express'),('DN','Diners'),('TD','Tarjetas Débito'),('CB','Consignación Bancaria'),('PB','Pagos via Baloto'),('EF','Efecty'))
+    TYPE_WAYTOPAY = models.TextField(max_length=1,choices=WAY_TO_PAY)
+    PRODUCT_QUALIFICATION = (('E','Excelente'),('P','Promedio'),('T','Terrible'))
+    TYPE_PRODUCTQUALIFICATION = models.TextField(max_length=1,choices=PRODUCT_QUALIFICATION)
+    SERVICE_QUALIFICATION = (('S','Satisfecho'),('N','Neutral'),('I','Insatisfecho'))
+    TYPE_SERVICEQUALIFICATION = models.TextField(max_length=1,choices=SERVICE_QUALIFICATION)
